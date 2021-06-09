56579c93b16b845f98fa14cefad0f46c30239c37
Manish Sharma <manish.sharma2464@gmail.com>
2021-06-09 13:46:49 +0530

    remove all app stop scripts

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -5,22 +5,22 @@
 # pm2 stop all
 # pm2 del all
 
-cd /home/ec2-user/demo
+# cd /home/ec2-user/demo
 
 # echo "Stopping existing Docker Containers"
 
 # docker-compose down
 
 # Removing / Purging all the images
-echo "Purging / Removing existing Docker Containers"
+# echo "Purging / Removing existing Docker Containers"
 
 # if [! docker ps -a -q && ! docker images -a -q] ; then
-if [[ ! $(docker ps -a -q) || ! $(docker images -a -q) ]] ; then
-    echo "No Docker Containers to Purge / Delete"
-else
-    docker rm -vf $(docker ps -a -q)
-    docker rmi -f $(docker images -a -q)
-fi
+# if [[ ! $(docker ps -a -q) || ! $(docker images -a -q) ]] ; then
+#     echo "No Docker Containers to Purge / Delete"
+# else
+#     docker rm -vf $(docker ps -a -q)
+#     docker rmi -f $(docker images -a -q)
+# fi
 
 
 # docker rm -vf $(docker ps -a -q)
